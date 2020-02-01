8f7b181f9458c30fcd7a01ba06011c5dd0afe43c
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-01 19:24:54 -0300

    Adicao do efeito typewritter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,55 @@ import React, { Component } from 'react'
 import { Container } from './styles'
 
 export default class Home extends Component {
+
+    deleteH1 = phrase => { //Funcao para deletar frase do dom
+        let h1 = document.querySelector('h1').innerHTML
+        let lenghtTitle = h1.length
+        let newh1 = ''
+        const interval = setInterval(() => {
+            for (let i = 0; i < h1.length-1; i++) { //Criando nova frase com o h1 exceto a ultima letra
+                newh1 += h1[i]
+            }
+            document.querySelector('h1').innerHTML = newh1 //Substituindo o texto
+            newh1 = ''
+            lenghtTitle--
+            if (lenghtTitle === 0) { //Se a frase ja foi deletada por completo, inicia a digitacao da proxima frase
+                clearInterval(interval)
+                this.typeH1(phrase + 1)
+            }
+            h1 = document.querySelector('h1').innerHTML
+        }, 25)
+    }
+    
+    typeH1 = (phrase = 0) => { //Funcao para digitar frase no dom
+        const titles = ['Lorem ipsum', 'is simply dummy text', 'of the printing and typesetting industry.'] //Frases a serem digitadas
+        if (phrase >= titles.length) {
+            phrase = 0
+        }
+        let h1 = document.querySelector('div h1#text')
+        console.log(h1)
+        let j = 0 //Contador para verificar se a frase ja chegou no final
+        const title = titles[phrase]
+        const lenghtTitle = title.length
+        const interval = setInterval(() => {
+            h1.innerHTML += title[j] //Adicionando letra no dom
+            j++
+            if (j >= lenghtTitle) { //Se a frase chegou no final o intervalo e interrompido para o h1 ser deletado na funcao deleteH1
+                clearInterval(interval)
+                setTimeout(this.deleteH1.bind(null, phrase), 2000)
+            }
+        }, 50)
+    }
+
+    componentDidMount() {
+        this.typeH1()
+    }
+
     render() {
+        
         return (
             <Container>
-                <h1>Oi</h1>
+                <h1 id="text">Oi</h1>
             </Container>
         )
     }
