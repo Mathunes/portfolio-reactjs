7d75404a87adafc2cb4c26a77949bf46152c2ed5
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-02-05 09:57:26 -0300

    Correcao na url de redicionamento para os projetos

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -14,42 +14,42 @@ export default class Projects extends Component {
             <Container>
                 <h1>Projetos</h1>
                 <ContainerProjects>
-                    <a href={process.env.PUBLIC_URL + 'https://consuming-vagalume-api-reactjs.herokuapp.com/'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://consuming-vagalume-api-reactjs.herokuapp.com/'} target="_blank" rel="noopener noreferrer">
                         <img src={vagalumeProject} alt="Projeto vagalume API" />
                         <HoverProject>
                             <h2>Vagalume Clone</h2>
                             <span>Aplicação desenvolvida para consumir dados da API do Vagalume com ReactJS</span>
                         </HoverProject>
                     </a>
-                    <a href={process.env.PUBLIC_URL + 'https://mathunes.github.io/consuming-github-api-react-redux/'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://mathunes.github.io/consuming-github-api-react-redux/'} target="_blank" rel="noopener noreferrer">
                         <img src={githubProject} alt="Projeto github API" />
                         <HoverProject>
                             <h2>Github Clone</h2>
                             <span>Aplicação desenvolvida para consumir dados da API do Github com ReactJS</span>
                         </HoverProject>
                     </a>
-                    <a href={process.env.PUBLIC_URL + 'https://mathunes.github.io/consuming-pixabay-api-react-redux/'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://mathunes.github.io/consuming-pixabay-api-react-redux/'} target="_blank" rel="noopener noreferrer">
                         <img src={pixabayProject} alt="Projeto pixabay API" />
                         <HoverProject>
                             <h2>Pixabay Clone</h2>
                             <span>Aplicação desenvolvida para consumir dados da API do Pixabay com ReactJS</span>
                         </HoverProject>
                     </a>
-                    <a href={process.env.PUBLIC_URL + 'https://mathunes.github.io/memory-game/'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://mathunes.github.io/memory-game/'} target="_blank" rel="noopener noreferrer">
                         <img src={memoryGameProject} alt="Projeto jogo da memória" />
                         <HoverProject>
                             <h2>Jogo da memória</h2>
                             <span>Jogo da memória desenvolvido com JavaScript</span>
                         </HoverProject>
                     </a>
-                    <a href={process.env.PUBLIC_URL + 'https://smashbug.000webhostapp.com/index.php'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://smashbug.000webhostapp.com/index.php'} target="_blank" rel="noopener noreferrer">
                         <img src={smashBugProject} alt="Projeto smashbug" />
                         <HoverProject>
                             <h2>Jogo SmashBug</h2>
                             <span>Jogo de esmagar insetos desenvolvido com HTML, CSS, JavaScript, JQuery e PHP</span>
                         </HoverProject>
                     </a>
-                    <a href={process.env.PUBLIC_URL + 'https://mathunes.github.io/timeline/'} target="_blank" rel="noopener noreferrer">
+                    <a href={'https://mathunes.github.io/timeline/'} target="_blank" rel="noopener noreferrer">
                         <img src={timeLineProject} alt="Projeto timeline" />
                         <HoverProject>
                             <h2>Linha do tempo</h2>
